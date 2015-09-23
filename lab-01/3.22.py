75b653c76964901d84771965c2d13bd8a7c9bf66
OlyaSavitska <olyasavitska23@gmail.com>
2015-09-23 19:16:07 +0300

    Create 3.22.py

---
+++ lab-01/3.22.py
@@ -0,0 +1,7 @@
+words = ['newly', 'formed', 'bland', 'ideas', 'are',
+         'expressible', '', 'an', 'furiatg', 'way']
+
+#використала метод для перетворення масиву в стрічку
+s = " ".join(words)
+
+print s
