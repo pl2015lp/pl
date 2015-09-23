9c4eddcf938127d6c693d02b676e01030b61bfe0
OlyaSavitska <olyasavitska23@gmail.com>
2015-09-23 18:44:51 +0300

    Create 3.16.py

---
+++ lab-01/3.16.py
@@ -0,0 +1,11 @@
+words = ["hi", "my", "buy", "son"]
+print words
+
+# функція sorted повертає відсортований масив,
+# не змінюючи при цьому заданий масив"
+print sorted(words)
+print words
+
+# метод sort() модифікує масив від якого його викликали
+words.sort()
+print words
