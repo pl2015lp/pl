f1b628923dbde044bfa1191f578e159763851685
OlyaSavitska <olyasavitska23@gmail.com>
2015-09-24 12:13:55 +0300

    Update 3.12.py

--- lab-01/3.12.py
+++ lab-01/3.12.py
@@ -1,7 +1,11 @@
-userInfo = ["Olya", "Savitska", "Yuriivna"]
+#Представити прізвище, ім'я та по батькові як список стрічок. Розділити речення на мокремі елементи, межа рорзділу голосна
+буква
 
+userInfo = ["Olya", "Savitska", "Yuriivna"]
 for i in range(len(userInfo)):
     # заміняємо слово на окремі елементи з межою розділення "і"
     userInfo[i] = userInfo[i].split("i")
-
 print userInfo
+[['Olya'], 'Savitska', 'Yuriivna']
+[['Olya'], ['Sav', 'tska'], 'Yuriivna']
+[['Olya'], ['Sav', 'tska'], ['Yur', '', 'vna']]
