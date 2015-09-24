286e5bbfdcd5ea1ce9dd5ab11a0c285d12fc9ae3
OlyaSavitska <olyasavitska23@gmail.com>
2015-09-24 12:18:41 +0300

    Update 3.16.py

--- lab-01/3.16.py
+++ lab-01/3.16.py
@@ -1,11 +1,15 @@
+#Створити змінну words яка містить список слів. Дослідіть операції words.sort() i sorted(words)
+
 words = ["hi", "my", "buy", "son"]
 print words
-
+['hi', 'my', 'buy', 'son']
 # функція sorted повертає відсортований масив,
 # не змінюючи при цьому заданий масив"
 print sorted(words)
+['buy', 'hi', 'my', 'son']
 print words
-
+['hi', 'my', 'buy', 'son']
 # метод sort() модифікує масив від якого його викликали
 words.sort()
 print words
+['buy', 'hi', 'my', 'son']
