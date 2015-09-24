c100d322d113f3b160b5b5fbdbba5c9523db7ee0
OlyaSavitska <olyasavitska23@gmail.com>
2015-09-24 12:48:51 +0300

    Update 3.6.py

--- lab-01/3.6.py
+++ lab-01/3.6.py
@@ -1,3 +1,6 @@
+#визначити стрічку s='colorless'. Використовуючи зрізи та операції поєднання змінити стрічку до вигляду "colourless"
+
 s = 'colorless'
 new_s = s[:4] + 'u' + s[4:]
 print new_s
+colourless
