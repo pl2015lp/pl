ba9b058cff3df9b21d206b89dce910bc51b274ec
OliaTurchyniak <oljalja02@gmail.com>
2015-09-30 22:51:34 +0300

    Laba1
    The first lab

---
+++ Lab1/3.25.py
@@ -0,0 +1,13 @@
+Python 2.7.3 (default, Apr 10 2012, 23:31:26) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Визначіть позиції всіх слів в списку phrase1 використовуючи метод index().
+>>> 
+>>> phrase1 = ['mother', 'father', 'brother']
+>>> for word in phrase1:
+	print word, phrase1.index(word)
+
+	
+mother 0
+father 1
+brother 2
+>>> 
---
+++ Lab1/3.3.py
@@ -0,0 +1,12 @@
+Python 2.7.3 (default, Apr 10 2012, 23:31:26) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Здійснити арифметичні операції зі стрічкою msg
+>>> 
+>>> msg = "Olha Turchyniak"
+>>> "Olha Turchyniak".count("a")
+2
+>>> "Olha Turchyniak".count("t")
+0
+>>> "Olha Turchyniak".count("l")
+1
+>>> 
