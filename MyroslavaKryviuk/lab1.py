36843c37bd3fe0b4d517a2e72ccdb7c833267aa3
MyroslavaKryviuk <krevuk@gmail.com>
2015-12-02 23:39:57 +0200

    lab7

--- MyroslavaKryviuk/lab1.py
+++ MyroslavaKryviuk/lab1.py
@@ -1,33 +1,9 @@
-msg='Myroslava Kryviuk'
-print (msg)
+str = 'My name is Myroslava'
+sent = str.split()
+my_list = list()
 
-a = msg [1::2]
-print (a)
+for i in reversed(sorted(sent, key=len)):
+   my_list.append(i)
 
-s='colorless'
-print (s)
-b=s[0:4]+'u'+s[4:]
-print (b)
+print(my_list)
 
-phrase1=['Myroslava', 'Petrivna', 'Kryviuk']
-print (phrase1)
-print (phrase1[2][2])
-
-lengths=[]
-for word in phrase1:
-    print len(word),word
-
-for word in phrase1:
-    lengths.append(len(word))
-
-print(lengths)
-
-silly='newly formed bland ideas are inexpressible in an infuriating way'
-silly = silly.split (" ")
-silly.sort()
-print (silly)
-filtered = []
-for word in silly:
-    if word !="in":
-        filtered.append(word)
-print(filtered)
