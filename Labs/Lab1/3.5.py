919d67cc0419182ff0eff0b0df51591b6f5e082e
Liudmyla-Artym <nameofuser4@gmail.com>
2015-12-25 03:04:26 +0200

    11, 12

--- Labs/Lab1/3.5.py
+++
@@ -1,8 +0,0 @@
-Python 2.7.3 (default, Apr 10 2012, 23:31:26) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> # Використовуючи зрізи та операцію поєднання змінити стрічку msg до вигляду ім’я , по батькові, прізвище студента.
->>> msg = "OlhaTurchyniak"
->>> msg = msg [0:4] + ' ' + 'Ivanivna' + ' ' + msg[4:14]
->>> msg
-'Olha Ivanivna Turchyniak'
->>> 
